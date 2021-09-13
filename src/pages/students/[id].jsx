7c0dda090bf45de985233ada9ae529708147169b
Eduardo Elly <eduardo_elly@hotmail.com>
2021-09-13 18:34:58 -0300

    Using - getStaticPaths

---
+++ src/pages/students/[id].jsx
@@ -0,0 +1,42 @@
+export async function getStaticPaths() {
+    const resp = await fetch('http://localhost:3000/api/students/tutors')
+    const ids = await resp.json()
+
+    const paths = ids.map(id => {
+        return { params: { id: `${id}` } }
+    })
+
+    return {
+        fallback: true, // false => 404
+        paths
+    }
+}
+
+export async function getStaticProps({ params }) {
+    console.log(params)
+    const resp = await fetch(`http://localhost:3000/api/students/${params.id}`)
+    const student = await resp.json()
+
+    return {
+        props: {
+            student: student
+        }
+    }
+}
+
+export default function StudentById(props) {
+    const { student } = props
+    return (
+        <div>
+            <h1>Student Details</h1>
+            {student ?
+                <ul>
+                    <li>{student.id}</li>
+                    <li>{student.name}</li>
+                    <li>{student.email}</li>
+                </ul>
+                : false
+            }
+        </div>
+    )
+}
